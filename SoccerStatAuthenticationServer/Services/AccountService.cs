4bd85aef5fd670cdc0d9bf203e1038ccf586807c
Dmitriy Permyakov <dmitrypermyakov1990@gmail.com>
2022-03-20 13:35:53 +0300

    Initialized services

---
+++ SoccerStatAuthenticationServer/Services/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using SoccerStatAuthenticationServer.DTOs.Requests;
+using SoccerStatAuthenticationServer.DTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerStatAuthenticationServer.Services
+{
+    public class AccountService : IAccountService
+    {
+        public Task<AuthenticationResult> Login(LoginRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthenticationResult> RefreshToken(RefreshTokenRequest refreshTokenRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthenticationResult> Register(RegisterRequest registerRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoccerStatAuthenticationServer/Services/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using SoccerStatAuthenticationServer.DTOs.Requests;
+using SoccerStatAuthenticationServer.DTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerStatAuthenticationServer.Services
+{
+    public interface IAccountService
+    {
+        public Task<AuthenticationResult> Register(RegisterRequest registerRequest);
+        public Task<AuthenticationResult> Login(LoginRequest loginRequest);
+        public Task<AuthenticationResult> RefreshToken(RefreshTokenRequest refreshTokenRequest);
+    }
+}
