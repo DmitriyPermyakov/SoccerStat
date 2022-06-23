31963fbc47f76c5dd647ecd5c0b8f4ddaf4751f9
unknown <dmitrypermyakov1990@gmail.com>
2022-06-23 14:11:17 +0500

    Basic project settings in startup

--- SoccerStatResourceServer/Startup.cs
+++ SoccerStatResourceServer/Startup.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using SoccerStatResourceServer.AuthenticationSettings;
+using SoccerStatResourceServer.Repository;
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +21,7 @@ namespace SoccerStatResourceServer
 {
     public class Startup
     {
+        private readonly string localhostConnection = "localhostConnection";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,10 +34,58 @@ namespace SoccerStatResourceServer
         {
 
             services.AddControllers();
+            string connectionString = Configuration.GetConnectionString("ResourceDatabase");
+            services.AddDbContext<ResourceDbContext>(options => options.UseSqlServer(connectionString));
+
+            JwtSettings jwtSettings = new JwtSettings();
+            Configuration.Bind("JwtSettings", jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            TokenValidationParameters tokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = false,
+                ValidateLifetime = false,
+
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.RefreshTokenSecret))
+            };
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: localhostConnection, builder => builder.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .WithMethods("PUT", "POST", "GET", "DELETE"));
+            });
+            #region
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SoccerStatResourceServer", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SoccerStatAuthenticationServer", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter into fiels the word 'Bearer' following by space and JWT",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +103,12 @@ namespace SoccerStatResourceServer
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthorization();
+            app.UseCors(localhostConnection);
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers().RequireCors(localhostConnection);
             });
         }
     }
