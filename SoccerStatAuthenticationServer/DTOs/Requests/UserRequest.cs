c37ca6650fc6103a797490b191ccc9a7f58b7bf3
Dmitriy Permyakov <dmitrypermyakov1990@gmail.com>
2022-06-29 16:51:47 +0300

    Implemented UserController

---
+++ SoccerStatAuthenticationServer/DTOs/Requests/UserRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SoccerStatAuthenticationServer.DomainObjects;
+
+namespace SoccerStatAuthenticationServer.DTOs.Requests
+{
+    public class UserRequest
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        public ICollection<Role> Roles { get; set; }
+    }    
+}
